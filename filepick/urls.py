9358cb3c9c979b07e10843cf24d49522c1922faf
Moonjuun <cmoonjun11@gmail.com>
2025-05-27 23:04:45 +0900

    ADD : 이미지 리사이즈 기능 추가

--- filepick/urls.py
+++ filepick/urls.py
@@ -22,5 +22,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
+    path('api/image/', include('image_tools.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
